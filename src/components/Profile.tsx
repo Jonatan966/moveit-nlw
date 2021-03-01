708a471566be677e1115bc6084462e42cde1df71
Jonatan966 <jonatanfrederico@gmail.com>
2021-03-01 19:40:49 -0300

    feature: implementação do AuthContext

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,32 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+import { AuthContext } from '../contexts/AuthContext';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
   const {level} = useContext(ChallengesContext);
+  const {getData} = useContext(AuthContext);
+
+  const [name, setName] = useState('');
+  const [avatar, setAvatar] = useState('');
+
+  useEffect(() => {
+    getData().then(item => {
+      setName(item.name);
+      setAvatar(item.avatar_url);
+    });  
+  }, []);
+
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/Jonatan966.png" alt="Jonatan F"/>
+      {!avatar 
+        ? <FaUserCircle color='var(--gray-line)' size={35}/> 
+        : <img src={avatar} alt="Jonatan F"/>
+      }
+      
       <div>
-        <strong>Jonatan Frederico</strong>
+        <strong>{name ? name : 'Buscando. . .'}</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
           Level {level}

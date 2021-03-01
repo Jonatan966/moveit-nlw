0da2d5647879ca93d70c45bcdf2970526b1a48d0
Jonatan966 <jonatanfrederico@gmail.com>
2021-03-01 13:50:14 -0300

    adjust: melhorias no input de login

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import {FiArrowRight} from 'react-icons/fi';
 
 import styles from '../styles/components/Input.module.css';
 
 function Input() {
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+
   return (
     <label className={styles.InputContainer}>
-      <input type="text"/>
-      <button>
+      <input 
+        type="text"
+        placeholder='Digite seu username'
+        onChange={obj => setIsButtonEnabled(!!obj.target.value)}
+        name='username'
+      />
+      <button 
+        disabled={!isButtonEnabled}
+        type='submit'
+      >
         <FiArrowRight size={25}/>
       </button>
     </label>

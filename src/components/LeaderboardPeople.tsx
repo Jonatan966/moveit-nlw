373adcad48b2a6f7b6647f2131dd1de20d1c8499
Jonatan966 <jonatanfrederico@gmail.com>
2021-03-01 19:37:34 -0300

    adjust: criação de um novo profile

--- src/components/LeaderboardPeople.tsx
+++ src/components/LeaderboardPeople.tsx
@@ -1,10 +1,26 @@
-import { Profile } from "./Profile";
 import styles from '../styles/components/LeaderboardPeople.module.css';
+import profileStyles from '../styles/components/Profile.module.css';
 
 interface LeaderboardPeopleProps {
   position: number;
   completedChallenges: number;
   experience: number;
+}
+
+function LeaderboardProfile() {
+  return (
+    <div className={profileStyles.profileContainer}>
+      <img src='https://github.com/Jonatan966.png' alt="Jonatan F"/>
+      
+      <div>
+        <strong>Teste</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
 }
 
 function LeaderboardPeople({position, completedChallenges, experience}: LeaderboardPeopleProps) {
@@ -14,7 +30,7 @@ function LeaderboardPeople({position, completedChallenges, experience}: Leaderbo
         <h1>{position}</h1>
       </span>
       <span>
-        <Profile/>
+        <LeaderboardProfile/>
       </span>
       <span>
         <strong>{completedChallenges}</strong>
